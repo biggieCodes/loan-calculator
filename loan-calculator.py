ee65de518ac3cb63f9f4efcec98ee6938d0c419c
Abdulsamad Rabo <raboabdulsamad04@gmail.com>
2023-11-06 00:08:49 +0100

    third commit

--- loan-calculator.py
+++ loan-calculator.py
@@ -45,6 +45,29 @@ class LoanCalculator:
 								command = self.computePayment).grid(
 								row = 6, column = 2, sticky = E) 
 		window.mainloop() # Create an event loop
+  
+  
+  
+  def computePayment(self):
+	# compute the total payment.
+	monthlyPayment = self.getMonthlyPayment(float(self.loanAmountVar.get()),
+					float(self.annualInterestRateVar.get()) / 1200,
+					int(self.numberOfYearsVar.get()))
+
+	self.monthlyPaymentVar.set(format(monthlyPayment, '10.2f'))
+	totalPayment = float(self.monthlyPaymentVar.get()) * 12 \
+						* int(self.numberOfYearsVar.get())
+	self.totalPaymentVar.set(format(totalPayment, '10.2f'))
+
+# compute the monthly payment.
+def getMonthlyPayment(self, loanAmount, monthlyInterestRate, numberOfYears): 
+	monthlyPayment = loanAmount * monthlyInterestRate 
+	(1- 1 / (1 + monthlyInterestRate) **
+	(numberOfYears * 12))
+
+	return monthlyPayment
+
+
 
 
 	# compute the total payment.
